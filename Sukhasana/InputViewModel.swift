36ff87c9c042ffe2b772a8b0e097e9078a29461c
Tom Brow <tom@tombrow.com>
2015-02-08 17:01:06 -0800

    API call for task results

--- Sukhasana/InputViewModel.swift
+++ Sukhasana/InputViewModel.swift
@@ -7,19 +7,56 @@
 //
 
 import ReactiveCocoa
+import Alamofire
 
 class InputViewModel {
   let textFieldText = MutableProperty("")
   
   init() {
-    let workspaceID = 15793206719
-    let typeaheadType = "task"
+    let taskRequests: SignalProducer<SignalProducer<NSDictionary, NSError>, NoError> = textFieldText.producer
+      |> map { text in requestTasks(text) }
     
-    let taskTypeaheadURLs: SignalProducer<NSURL, NoError> = textFieldText.producer
-      |> map { text in NSURL(string: "https://app.asana.com/api/1.0/workspaces/\(workspaceID)/typeahead?type=\(typeaheadType)&query=\(text)")! }
-    
-    taskTypeaheadURLs.start { next in
-      println("\(next)")
+    taskRequests.start { request in
+      request.start { dict in
+        println("\(dict)")
+      }
+      return
     }
   }
 }
+
+private let requestManager: Alamofire.Manager = {
+  let APIKey = "4cAUaVhk.Vt70w5u6rOHQgsy3fsLoX9v"
+  
+  let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+  config.HTTPAdditionalHeaders = ["Authorization": "Basic " + ("\(APIKey):" as NSString)
+    .dataUsingEncoding(NSUTF8StringEncoding)!
+    .base64EncodedStringWithOptions(NSDataBase64EncodingOptions.allZeros)]
+  
+  return Manager(configuration: config)
+}()
+
+private func requestTasks(query: String) -> SignalProducer<NSDictionary, NSError> {
+  let workspaceID = 14801884099708
+  let typeaheadType = "task"
+  
+  return SignalProducer { observer, _ in
+    requestManager
+      .request(
+        .GET,
+        "https://app.asana.com/api/1.0/workspaces/\(workspaceID)/typeahead",
+        parameters: ["type": typeaheadType, "query": query])
+      .responseJSON {_, _, JSON, error in
+        if let error = error {
+          sendError(observer, error)
+        } else {
+          if let dict = JSON as? NSDictionary {
+            sendNext(observer, dict)
+          }
+          sendCompleted(observer)
+        }
+    }
+    return
+  }
+}
+
