7bd4f7ea75b82e2842942fd8a713f417dc43e8df
Tom Brow <tom@tombrow.com>
2015-02-22 00:24:52 -0800

    Skip settings screen on launch if saved settings

--- Sukhasana/ApplicationModel.swift
+++ Sukhasana/ApplicationModel.swift
@@ -17,24 +17,35 @@ struct ApplicationModel {
   let shouldDisplayScreen: SignalProducer<Screen, NoError>
   
   init(settingsStore: SettingsStore) {
-    let (settingsModel, didSaveSettings) = SettingsScreenModel.makeWithSettings(settingsStore.restoreSettings())
+    let restoredSettings = settingsStore.restoreSettings()
+    let (settingsModel, didSaveSettings) = SettingsScreenModel.makeWithSettings(restoredSettings)
     
     // Persist saved settings
     didSaveSettings.start { settings in
       settingsStore.saveSettings(settings)
     }
     
+    // Show the main screen on launch if settings are restored
+    let didRestoreSettings: SignalProducer<Settings, NoError> = {
+      if let restoredSettings = restoredSettings {
+        return SignalProducer(value: restoredSettings)
+      } else {
+        return SignalProducer.empty
+      }
+    }()
+    
     // Show the main screen after settings are saved
-    let mainModelAndDidClickSettingsButton = didSaveSettings
+    let mainModelAndDidClickSettingsButton = didRestoreSettings
+      |> concat(didSaveSettings)
       |> map(MainScreenModel.makeWithSettings)
       |> replay(capacity: 1)
     let shouldDisplayMainScreen = mainModelAndDidClickSettingsButton
       |> map { (mainModel, _) in Screen.Main(mainModel) }
     
-    // Show the settings screen immediately and whenever the Settings button is clicked
-    let shouldDisplaySettingsScreen = mainModelAndDidClickSettingsButton
-      |> latestMap { (_, didClickSettingsButton) in didClickSettingsButton }
-      |> startWith(())
+    // Show the settings screen after the Settings button is clicked, and on
+    // launch if no settings have been restored
+    let shouldDisplaySettingsScreen = SignalProducer(values: restoredSettings == nil ? [()] : [])
+      |> concat (mainModelAndDidClickSettingsButton |> latestMap { $0.1 })
       |> map { _ in Screen.Settings(settingsModel) }
   
     shouldDisplayScreen = SignalProducer(values: [shouldDisplayMainScreen, shouldDisplaySettingsScreen])
